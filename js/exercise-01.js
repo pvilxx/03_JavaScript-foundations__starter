959b7d7f0dd8e4ac567beeedbe0d767b29a9c85e
pvilxx <pau.vilardell@mataro.epiaedu.cat>
2024-11-11 19:06:19 +0100

    Exercise 01 solved

--- js/exercise-01.js
+++ js/exercise-01.js
@@ -9,3 +9,18 @@
       Log 'name' value.
 
 */
+
+let name = "Pau"; 
+
+function updateValue(value) { 
+  name = value;
+}
+
+updateValue("Korn"); 
+console.log(name);
+
+updateValue(true); 
+console.log(name);
+
+updateValue(28);
+console.log(name);
